33b0a77d5121456d94fb72957a7dfb13e1b5776a
milkhouse_pc <kurokun@163.com>
2018-02-13 15:18:37 +0800

    boss hp gauge

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     // private bool ready;
-    public GameObject boss_hp_guage;
-
+    private BossHpGuage bossHpGuage;
+    void Awake()
+    {
+        bossHpGuage = GameObject.Find("ACTManager").transform.Find("ACTCanvas").Find("BossHpGauge").GetComponent<BossHpGuage>();
+    }
     public void GetReady()
     {
+        bossHpGuage.gameObject.SetActive(true);
         // ready = true;
         // GetComponent<Enemy1>().enabled = true;
-        Instantiate(boss_hp_guage);
     }
 }
---
+++ Assets/Scripts/BossHpGuage.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHpGuage : MonoBehaviour
+{
+    public int hp = 64;
+    private Slider slider;
+    public Sprite lifeTexture0;
+    public Sprite lifeTexture1;
+    public Sprite lifeTexture2;
+    private Image backImage;
+    private Image fillImage;
+
+    private Boss boss;
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+        backImage = transform.Find("Background").GetComponent<Image>();
+        fillImage = slider.fillRect.GetComponent<Image>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boss = FindObjectOfType<Boss>();
+        if (boss)
+            hp = boss.GetComponent<Status>().GetHp();
+        else
+            hp = 0;
+        if (hp > 32)
+        {
+            transform.Find("Background").GetComponent<Image>().sprite = lifeTexture1;
+            slider.fillRect.GetComponent<Image>().sprite = lifeTexture2;
+            slider.value = hp - 32;
+        }
+        else
+        {
+            backImage.sprite = lifeTexture0;
+            fillImage.sprite = lifeTexture1;
+            slider.value = hp;
+        }
+    }
+}
