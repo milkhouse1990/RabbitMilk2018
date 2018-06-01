b2743e0e525171d602b4a8e13c553ee40ef77be9
milkhouse <kurokun@163.com>
2018-06-01 18:36:32 +0800

    [Change] 开关的位置参数变为public

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -2,41 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch : MonoBehaviour {
+public class Switch : MonoBehaviour
+{
     private Transform switch_up;
     private Transform switch_down;
     private Transform platform;
     private bool working;
     private bool up;
-	// Use this for initialization
-	void Start () {
+    public float highestHeight;
+    public float lowestHeight;
+    // Use this for initialization
+    void Start()
+    {
         switch_up = transform.Find("switch_up");
         switch_down = transform.Find("switch_down");
         platform = transform.Find("platform");
         working = false;
         up = GetComponent<Animator>().GetBool("up");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (platform.position.y>14)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (platform.position.y > highestHeight)
         {
-            platform.position = new Vector3(platform.position.x, 14f, platform.position.z);
+            platform.position = new Vector3(platform.position.x, highestHeight, platform.position.z);
             platform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             working = false;
         }
-        if (platform.position.y<3.25)
+        if (platform.position.y < lowestHeight)
         {
-            platform.position = new Vector3(platform.position.x, 3.25f, platform.position.z);
+            platform.position = new Vector3(platform.position.x, lowestHeight, platform.position.z);
             platform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             working = false;
         }
-	}
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!working)
         {
-            
+
             if (other.tag == "weapon")
             {
                 up = !up;
@@ -47,9 +52,9 @@ public class Switch : MonoBehaviour {
                 else
                     platform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 2f);
             }
-                
+
         }
-        
+
 
     }
 }
